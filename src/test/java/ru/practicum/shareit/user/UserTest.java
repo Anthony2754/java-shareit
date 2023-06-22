07716b1f16e9d5297435e288ff25238fc803f2d3
Anton Bobrov <anthony020@yandex.ru>
2023-06-22 11:56:52 +0300

    fix: tests

---
+++ src/test/java/ru/practicum/shareit/user/UserTest.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.practicum.shareit.user.model.User;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserTest {
+    private final User userOne = User.builder()
+            .id(1L)
+            .name("User")
+            .email("email@mail.ru")
+            .build();
+    private final User userTwo = User.builder()
+            .id(1L)
+            .name("User")
+            .email("email@mail.ru")
+            .build();
+
+    @Test
+    void equalsTo() {
+        assertNotEquals(null, userOne);
+        assertEquals(userOne, userOne);
+        assertEquals(userOne, userTwo);
+    }
+}

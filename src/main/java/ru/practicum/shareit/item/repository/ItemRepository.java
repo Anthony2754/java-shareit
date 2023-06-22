7e11bb41344ca7a23346aa33ac0eda5d2d950e06
Anton Bobrov <anthony020@yandex.ru>
2023-06-22 09:12:06 +0300

    style

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.item.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
